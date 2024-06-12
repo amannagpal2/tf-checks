9d40c28879b11f62bb78bacb9ffdf7e844b54009
Aman42988 <97885282+Aman42988@users.noreply.github.com>
2024-06-12 10:15:36 +0530

    Update c1-versions.tf

--- Terraform_code/c1-versions.tf
+++ Terraform_code/c1-versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source = "hashicorp/static"
       version = ">= 2.0" 
     }
     random = {

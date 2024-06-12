30d56dd7ea531dd86d142d891482bf8d446e174a
Aman42988 <97885282+Aman42988@users.noreply.github.com>
2024-06-12 10:47:07 +0530

    Update c3-virtual-network.tf

--- Terraform_code/c3-virtual-network.tf
+++ Terraform_code/c3-virtual-network.tf
@@ -1,32 +1,32 @@
-# Create Virtual Network (Exposed to Internet)
+# Create Virtual Network
 resource "azurerm_virtual_network" "myvnet" {
   name                = "myvnet-1"
-  address_space       = ["0.0.0.0/0"]  # Exposing to the entire internet
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
 }
 
-# Create Subnet (Exposed to Internet)
+# Create Subnet
 resource "azurerm_subnet" "mysubnet" {
   name                 = "mysubnet-1"
   resource_group_name  = azurerm_resource_group.myrg.name
   virtual_network_name = azurerm_virtual_network.myvnet.name
-  address_prefixes     = ["0.0.0.0/0"]  # Exposing to the entire internet
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
-# Create Public IP Address (Static)
+# Create Public IP Address
 resource "azurerm_public_ip" "mypublicip" {
   name                = "mypublicip-1"
   resource_group_name = azurerm_resource_group.myrg.name
   location            = azurerm_resource_group.myrg.location
-  allocation_method   = "Static"  # Using a static IP address
-  domain_name_label   = "app1-vm-${random_string.myrandom.id}"
+  allocation_method   = "Static"
+  domain_name_label = "app1-vm-${random_string.myrandom.id}"
   tags = {
     environment = "Dev"
   }
 }
 
-# Create Network Interface (Exposed to Internet)
+# Create Network Interface
 resource "azurerm_network_interface" "myvmnic" {
   name                = "vmnic"
   location            = azurerm_resource_group.myrg.location
@@ -36,6 +36,11 @@ resource "azurerm_network_interface" "myvmnic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.mysubnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.mypublicip.id  # Exposing to the entire internet
+    public_ip_address_id = azurerm_public_ip.mypublicip.id 
   }
+}
+resource "azurerm_network_security_rule" "vulnerable_example" {
+    direction                  = "Inbound"
+    destination_address_prefix = "0.0.0.0/0"  # All traffic allowed
+    access                     = "Allow"
 }

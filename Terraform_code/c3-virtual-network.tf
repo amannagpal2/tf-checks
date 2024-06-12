20cf1f6fa44000c39e123b628c4ea131aacb4a5b
Aman42988 <97885282+Aman42988@users.noreply.github.com>
2024-06-12 10:27:05 +0530

    Update c3-virtual-network.tf

--- Terraform_code/c3-virtual-network.tf
+++ Terraform_code/c3-virtual-network.tf
@@ -1,7 +1,7 @@
 # Create Virtual Network
 resource "azurerm_virtual_network" "myvnet" {
   name                = "myvnet-1"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["0.0.0.0/0"]
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
 }
@@ -11,7 +11,7 @@ resource "azurerm_subnet" "mysubnet" {
   name                 = "mysubnet-1"
   resource_group_name  = azurerm_resource_group.myrg.name
   virtual_network_name = azurerm_virtual_network.myvnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["0.0.0.0/0"]
 }
 
 # Create Public IP Address
